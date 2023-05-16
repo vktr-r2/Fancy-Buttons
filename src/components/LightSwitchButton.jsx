204f5a6b5dca42986050fee39bb9c36b6522e7e4
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-16 14:57:45 -0400

    Add conditional rendering to LightSwtichButton component

--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -5,14 +5,16 @@ const LightSwitchButton = () => {
 
   return (
     <button className="LightSwitchButton">
-      {/*When the state is ON*/}{" "}
-      <span className="on">
-        <i>💡</i>I'm ON!
-      </span>
-      {/*When the state is OFF*/}{" "}
-      <span className="off">
-        <i>💡</i>I'm OFF!
-      </span>
+      {lightStatus === "on" && (
+        <span className="on">
+          <i>💡</i>I'm ON!
+        </span>
+      )}
+      {lightStatus === "off" && (
+        <span className="off">
+          <i>💡</i>I'm OFF!
+        </span>
+      )}
     </button>
   );
 };
