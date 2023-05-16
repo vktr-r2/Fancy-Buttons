aff7271d88f60d3d4e6bf53e64b603d027305946
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-16 14:27:18 -0400

    Create and export TextRepeaterButton component

---
+++ src/components/TextRepeaterButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextRepeaterButton = () => {
+  return (
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+    </button>
+  );
+}
+
+export default TextRepeaterButton;
