69db997bda8db5ef54af7a4ed9463ca2d26a02ee
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-16 15:30:23 -0400

    Implement functionality for handleClick function to update state of AngryButton component

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -3,10 +3,18 @@ import React, { useState } from "react";
 function AngryButton() {
   const [anger, setAnger] = useState(0);
 
+  const handleClick = (amount) => {
+    anger < 1 ? setAnger(anger + amount) : setAnger(0);
+  };
+
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+    <button
+      onClick={() => handleClick(0.1)}
+      style={{ backgroundColor: `rgba(255,0,0,${anger})` }}
+      className="AngryButton"
+    >
       {anger < 1 && <span>I'm warning you, don't push me too much!</span>}
-      {anger > 1 && <span>Rawr!</span>}
+      {anger > 1 && <span>You little $#!+, I WARNED YOU!</span>}
     </button>
   );
 }
