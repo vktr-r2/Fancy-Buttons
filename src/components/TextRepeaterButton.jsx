97eaa8926012c95f5aef28179ba6955826dd7823
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-16 15:57:53 -0400

    Implement functionality for handleClick function to update state of TextRepeaterButton component

--- src/components/TextRepeaterButton.jsx
+++ src/components/TextRepeaterButton.jsx
@@ -2,14 +2,21 @@ import React, { useState } from "react";
 
 const TextRepeaterButton = () => {
   const [repetitions, setRepititions] = useState(1);
-  const repetitionsArray = [];
 
+  const handleClick = () => {
+    setRepititions(repetitions + 1);
+  }
+
+  const repetitionsArray = [];
   for (let i = repetitions; i > 0; i--) {
     repetitionsArray.push(<span key={i}> I like this text!</span>)
   }
 
   return (
-    <button className="TextRepeaterButton">
+    <button 
+    onClick={() => handleClick()}
+    className="TextRepeaterButton"
+    >
       {repetitionsArray}
     </button>
   );
