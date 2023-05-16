c476e42d6d25abf172e4b7b7eb866b0d5642a0e3
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-16 14:19:51 -0400

    Create and export CounterButton component

---
+++ src/components/CounterButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CounterButton = () => {
+  
+  return (
+    <button className="CounterButton">
+      You clicked me X amount of times
+    </button>
+  );
+}
+
+export default CounterButton;
