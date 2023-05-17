185311f9373ea03fd83692347c363f6113783a35
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-16 21:16:28 -0400

    Refactor AngryButton component to use useReducer to check and change state

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+// Import necessary functions from React library
+import React, { useReducer, useState } from "react";
 
 function AngryButton() {
-  const [anger, setAnger] = useState(0);
 
-  const handleClick = (amount) => {
-    anger < 1 ? setAnger(anger + amount) : setAnger(0);
-  };
+  // Use the useReducer hook to handle state (anger) and dispatch actions
+  // Initial state of anger is 0
+  const [anger, dispatch] = useReducer((anger, action) => {
+    // The reducer function is called with the current state (anger) and an action
+    // If anger > 1, it resets to 0, otherwise it increments by the value of action
+    return anger > 1 ? 0 : anger + action;
+  }, 0)
 
+  // Return the JSX to be rendered
   return (
+    // Define a button element
     <button
-      onClick={() => handleClick(0.1)}
+      // On click, dispatch an action with value 0.1
+      onClick={() => dispatch(0.1)}
+      // The background color changes based on the state of anger, with the RGBA red channel increasing as anger increases
       style={{ backgroundColor: `rgba(255,0,0,${anger})` }}
       className="AngryButton"
     >
+      {/*// If anger is less than 1, display a warning message*/}
       {anger < 1 && <span>I'm warning you, don't push me too much!</span>}
+      {/*// If anger is greater than 1, display an angry message*/}
       {anger > 1 && <span>You little $#!+, I WARNED YOU!</span>}
     </button>
   );
